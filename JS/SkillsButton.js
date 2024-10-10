98c48f613cdfc3ea8a60c984203c61dc580b9877
simhana <daum_c0m@naver.com>
2024-10-10 10:44:51 +0900

    skills button

--- JS/SkillsButton.js
+++ JS/SkillsButton.js
@@ -1,30 +1,41 @@
-// 버튼 클릭하기
+let activeButton = null;
+
 window.addEventListener('DOMContentLoaded', function() {
-    showContent('language');
-  });
-  
-function showContent(content) {
+    showContent('language', document.getElementById('btn-language'));
+});
+
+function showContent(content, button) {
   var language = document.getElementById("skill-Language");
-  var tool = document.getElementById("skill-Tool")
+  var tool = document.getElementById("skill-Tool");
   var doc = document.getElementById("skill-Document");
   var design = document.getElementById("skill-Design");
   var community = document.getElementById("skill-Communication");
-  
+
   language.style.display = "none";
-  doc.style.display = "none";
   tool.style.display = "none";
+  doc.style.display = "none";
   design.style.display = "none";
   community.style.display = "none";
-  
+
   if (content === "language") {
     language.style.display = "flex";
   } else if (content === "tool") {
-      tool.style.display = "flex";
-  } else if (content === "document"){
-      doc.style.display = "flex";
-  } else if(content === "design"){
-      design.style.display ="flex";
-  } else if(content === "communication"){
-      community.style.display = "flex";
+    tool.style.display = "flex";
+  } else if (content === "document") {
+    doc.style.display = "flex";
+  } else if (content === "design") {
+    design.style.display = "flex";
+  } else if (content === "communication") {
+    community.style.display = "flex";
   }
+
+  if (activeButton) {
+    activeButton.style.backgroundColor = "";
+    activeButton.style.color = "";
+  }
+
+  button.style.backgroundColor = "#f5f5f5";
+  button.style.color = "#5C6B81"
+
+  activeButton = button;
 }
